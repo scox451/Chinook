88756c9c659f988b041934a5ad9230d4d9b2d298
Stephen Cox <scox451@yahoo.com>
2021-11-20 03:44:35 -0600

    uncomment code

--- Core/ChinookContext.cs
+++ Core/ChinookContext.cs
@@ -9,7 +9,7 @@ namespace Chinook.Core
     {
         public DbSet<Album> Albums { get; set; }
         public DbSet<Artist> Artists { get; set; }
-        // public DbSet<Customer> Customers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Invoice> Invoices { get; set; }
